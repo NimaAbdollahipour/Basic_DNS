dd7fa03f8c6a73b087aba49d12a457e4359d3901
Nima Abdollahipour <nima.abdollahi.a@gmail.com>
2021-11-08 08:05:11 -0800

    Updating DNS part of the client
    Changing the structure of the DNS function
    Adding new variables (for DNS IP and Port) +
    changing the Ports

--- client.py
+++ client.py
@@ -1,21 +1,21 @@
 import socket
 
 HEADER = 64
-PORT = 5050
+PORT = 18080
 SERVER = "127.0.0.1"
-#SERVER = socket.gethostbyname(socket.gethostname)
+#SERVER = socket.gethostbyname('localhost')
 ADDR = (SERVER,PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MSG = "!DISCONNECT"
+DNS_IP = "127.0.0.1"
+DNS_PORT = 15353
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def DNS():
+def DNS(name):
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	msg = input("[ENTER WEBSITE NAME]: ")
-	ip = socket.gethostbyname(socket.gethostname())
-	s.sendto(msg.encode("utf-8"),(ip,13345))
+	s.sendto(name.encode("utf-8"),(DNS_IP,DNS_PORT))
 	data, addr = s.recvfrom(512)
 	print("[SERVER MESSAGE]", data.decode())
 	s.close()
@@ -28,5 +28,5 @@ def HTTP(msg):
 	client.send(send_length)
 	client.send(message)
 
-
+DNS()
 HTTP("Hello")
